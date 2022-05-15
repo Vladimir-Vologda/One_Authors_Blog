490fb62eee093484bded58099d2b1d86ab089743
Vladimir-Vologda <vologda2018@inbox.ru>
2022-05-15 17:53:47 +0300

    Adding 'ArticleModel' to admin panel

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from articles.models import ArticleModel
+
+
+def get_title(self):
+    #   Функция, которая обрезает поле
+    #   до определённого количества символов
+    return self.title[:25]
+
+
+get_title.short_title = 'Title'
+
+
+@admin.register(ArticleModel)
+class ArticleAdminPanel(admin.ModelAdmin):
+    list_display = (
+        'id', get_title, 'creation_date', 'update_date', 'is_published',
+    )
+    list_display_links = (
+        get_title,
+    )
+    list_filter = (
+        'is_published',
+    )
+
+
+admin.site.unregister(Group)
