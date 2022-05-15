7fbfe08eb627236191eb3c1af4d6a5726850d314
Vladimir-Vologda <vologda2018@inbox.ru>
2022-05-15 23:57:50 +0300

    Creating a home page displaying published articles

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from articles.views import ListArticleView
+
 
 urlpatterns = [
-
+    path('', ListArticleView.as_view(), name='list_article'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import ListView
 
-# Create your views here.
+from articles.models import ArticleModel
+
+
+class ListArticleView(ListView):
+    model = ArticleModel
+    template_name = 'article/list_article.html'
+    context_object_name = 'list_article'
+
+    def get_queryset(self):
+        #   Функция, благодаря которой, будут передаваться
+        #   только опубликованные статьи (is_published=True)
+        return ArticleModel.objects.filter(is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ListArticleView, self).get_context_data(**kwargs)
+        context['title'] = _("Article's")
+        return context
+
